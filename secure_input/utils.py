64131d0abb6df30dceacb2a71c6713381ee80886
Victor Rocha <victor@rochapps.com>
2013-08-16 11:03:06 -0400

    Added a linkify filter to parsing links in the text.

--- secure_input/utils.py
+++ secure_input/utils.py
@@ -5,6 +5,7 @@ from .defaults import ALLOWED_ATTRIBUTES, ALLOWED_TAGS, ALLOWED_STYLES
 
 def safe_html(html):
     html = html or ''
-    cleaned_html = bleach.clean(html, tags=ALLOWED_TAGS,
+    linkified_html = bleach.linkify(html)
+    cleaned_html = bleach.clean(linkified_html, tags=ALLOWED_TAGS,
             attributes=ALLOWED_ATTRIBUTES, styles=ALLOWED_STYLES)
     return cleaned_html
